aec6f0df04aab0f349d16e15bd8178db0ad409c8
yarrami <yarrami@LFR019126.corp.capgemini.com>
2017-07-12 18:43:08 +0200

    update

--- src/main/java/testYAR/testWebService/client/RamWSRest.java
+++ src/main/java/testYAR/testWebService/client/RamWSRest.java
@@ -1,24 +1,22 @@
 package testYAR.testWebService.client;
 
-import org.springframework.web.context.request.WebRequest;
-
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
-
 public class RamWSRest {
-	private static String Url="http://dlap02.sibille.lan:8086/gestionadmin-api/services/rest/v1/adherents?nir=";
-	public static void getNumAdherents(String nir){
+	private static String Url = "http://dlap02.sibille.lan:8086/gestionadmin-api/services/rest/v1/adherents?nir=";
+
+	public static void getNumAdherents(String nir) {
 		Client clientRest = Client.create();
-		WebResource webResource = clientRest.resource(Url+nir);
+		WebResource webResource = clientRest.resource(Url + nir);
 		ClientResponse response = webResource.accept("application/json").get(ClientResponse.class);
-		if(response.getStatus()!=200){
-			throw new RuntimeException("HTTP Error: "+ response.getStatus());
+		if (response.getStatus() != 200) {
+			throw new RuntimeException("HTTP Error: " + response.getStatus());
 		}
-        String result = response.getEntity(String.class);
-        System.out.println("Response from the Server: ");
-        System.out.println(result);
+		String result = response.getEntity(String.class);
+		System.out.println("Response from the Server: ");
+		System.out.println(result);
 
 	}
 }
