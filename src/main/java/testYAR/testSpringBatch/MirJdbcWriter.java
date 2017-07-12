ee7952c0de98bf3c707a8a66270990b23e9cff1e
salifukayta <solsalim@gmail.com>
2017-07-12 09:05:59 +0200

    test2

--- src/main/java/testYAR/testSpringBatch/MirJdbcWriter.java
+++ src/main/java/testYAR/testSpringBatch/MirJdbcWriter.java
@@ -1,33 +1,33 @@
 package testYAR.testSpringBatch;
 
-
 import java.util.List;
 
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import testYAR.testSpringBatch.model.SngcDdf;
+
 import testYAR.testSpringBatch.model.SngcMirTrim;
 
-@Transactional (propagation=Propagation.REQUIRED, rollbackFor=Exception.class)
-public class MirJdbcWriter implements ItemWriter<SngcMirTrim>{
+@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+public class MirJdbcWriter implements ItemWriter<SngcMirTrim> {
 
-    private JdbcTemplate jdbcTemplate;
-    private static final String REQUEST_INSERT_MIR = "insert into MIR (type_enregistrement, zone_id_assure, zone_partenaire, zone_trimestres_regime, zone_anomalies_retours) values (?,?,?,?,?)";
+	private JdbcTemplate jdbcTemplate;
+	private static final String REQUEST_INSERT_MIR = "insert into MIR (type_enregistrement, zone_id_assure, zone_partenaire, zone_trimestres_regime, zone_anomalies_retours) values (?,?,?,?,?)";
 
-    public void write(List<? extends SngcMirTrim> items) throws Exception {
-        for (SngcMirTrim sngcMir : items) {
-            final Object object [] = { sngcMir.getTypeEnregistrement(), sngcMir.getZoneIdAssure(), sngcMir.getZonePartenaire(), sngcMir.getZoneTrimestresRegime(), sngcMir.getZoneAnomaliesRetours() };
-            jdbcTemplate.update(REQUEST_INSERT_MIR, object);
-        }
-    }
+	public void write(List<? extends SngcMirTrim> items) throws Exception {
+		for (SngcMirTrim sngcMir : items) {
+			final Object object[] = { sngcMir.getTypeEnregistrement(), sngcMir.getZoneIdAssure(), sngcMir.getZonePartenaire(),
+					sngcMir.getZoneTrimestresRegime(), sngcMir.getZoneAnomaliesRetours() };
+			jdbcTemplate.update(REQUEST_INSERT_MIR, object);
+		}
+	}
 
-    public JdbcTemplate getJdbcTemplate() {
-        return jdbcTemplate;
-    }
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
 
-    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 }
--- src/main/java/testYAR/testSpringBatch/MirProcessor.java
+++ src/main/java/testYAR/testSpringBatch/MirProcessor.java
@@ -2,42 +2,40 @@ package testYAR.testSpringBatch;
 
 import org.springframework.batch.item.ItemProcessor;
 
-import testYAR.testSpringBatch.model.SngcDdf;
 import testYAR.testSpringBatch.model.SngcMirTrim;
 
-public class MirProcessor implements ItemProcessor<SngcMirTrim, SngcMirTrim>
-{
+public class MirProcessor implements ItemProcessor<SngcMirTrim, SngcMirTrim> {
 	private final String MIR = "MIR";
 	private final String AIR = "AIR";
 	private final String TRIM = "TRIM";
 	private final String TRGL = "TRGL";
-    public SngcMirTrim process(final SngcMirTrim sngcMirInput) throws Exception
-    {
-    	SngcMirTrim sngcMirOutput=null;
-        if (estTypeEnregistrementOK(sngcMirInput) && estZoneTrimRegimeOK(sngcMirInput)) {
-        	sngcMirOutput = new SngcMirTrim();
-        	sngcMirOutput.setTypeEnregistrement(sngcMirInput.getTypeEnregistrement());
-        	sngcMirOutput.setZoneAnomaliesRetours(sngcMirInput.getZoneAnomaliesRetours());
-        	sngcMirOutput.setZoneIdAssure(sngcMirInput.getZoneIdAssure());
-        	sngcMirOutput.setZonePartenaire(sngcMirInput.getZonePartenaire());
-        	sngcMirOutput.setZoneTrimestresRegime(sngcMirInput.getZoneTrimestresRegime());
-        }else if (!estTypeEnregistrementOK(sngcMirInput)) {
-        	// log erreur :
-        	// - L’erreur « Anomalie de fichier aller SNGC 1 »
-        	// - Puis la ligne correspondante du fichier SNGC
-        }else if (!estZoneTrimRegimeOK(sngcMirInput)) {
-        	// log erreur :
-        	// - L’erreur « Ne concerne pas un droit »
-        	// - Puis la ligne correspondante du fichier SNGC
-        }
-        return sngcMirOutput;
-    }
-    
-    private boolean estTypeEnregistrementOK(final SngcMirTrim sngcMirInput) {
-    	return MIR.equals(sngcMirInput.getTypeEnregistrement()) || AIR.equals(sngcMirInput.getTypeEnregistrement());
-    }
-    
-    private boolean estZoneTrimRegimeOK(final SngcMirTrim sngcMirInput) {
-    	return sngcMirInput.getZoneTrimestresRegime().indexOf(TRIM)==0 || sngcMirInput.getZoneTrimestresRegime().indexOf(TRGL)==0;
-    }
+
+	public SngcMirTrim process(final SngcMirTrim sngcMirInput) throws Exception {
+		SngcMirTrim sngcMirOutput = null;
+		if (estTypeEnregistrementOK(sngcMirInput) && estZoneTrimRegimeOK(sngcMirInput)) {
+			sngcMirOutput = new SngcMirTrim();
+			sngcMirOutput.setTypeEnregistrement(sngcMirInput.getTypeEnregistrement());
+			sngcMirOutput.setZoneAnomaliesRetours(sngcMirInput.getZoneAnomaliesRetours());
+			sngcMirOutput.setZoneIdAssure(sngcMirInput.getZoneIdAssure());
+			sngcMirOutput.setZonePartenaire(sngcMirInput.getZonePartenaire());
+			sngcMirOutput.setZoneTrimestresRegime(sngcMirInput.getZoneTrimestresRegime());
+		} else if (!estTypeEnregistrementOK(sngcMirInput)) {
+			// log erreur :
+			// - L’erreur « Anomalie de fichier aller »
+			// - Puis la ligne correspondante du fichier SNGC
+		} else if (!estZoneTrimRegimeOK(sngcMirInput)) {
+			// log erreur :
+			// - L’erreur « Ne concerne pas un droit »
+			// - Puis la ligne correspondante du fichier SNGC
+		}
+		return sngcMirOutput;
+	}
+
+	private boolean estTypeEnregistrementOK(final SngcMirTrim sngcMirInput) {
+		return MIR.equals(sngcMirInput.getTypeEnregistrement()) || AIR.equals(sngcMirInput.getTypeEnregistrement());
+	}
+
+	private boolean estZoneTrimRegimeOK(final SngcMirTrim sngcMirInput) {
+		return sngcMirInput.getZoneTrimestresRegime().indexOf(TRIM) == 0 || sngcMirInput.getZoneTrimestresRegime().indexOf(TRGL) == 0;
+	}
 }
