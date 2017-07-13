4f4bd80857bece6bcbb43bec777a49b7022f4e70
salifukayta <solsalim@gmail.com>
2017-07-13 09:36:35 +0200

    -save for merge

---
+++ src/main/java/controller/TestController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import java.util.logging.Logger;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import testYAR.testSpringBatch.model.SngcMirTrim;
+
+@Path("/test")
+@Produces("application/json")
+public class TestController {
+
+	final static Logger log = Logger.getLogger("TestController");
+
+	@Path("/{id}")
+	@GET
+	public SngcMirTrim getTrooper(@PathParam("id") String id) {
+		log.info("l'id reçu est " + id);
+		return new SngcMirTrim();
+	}
+
+}
